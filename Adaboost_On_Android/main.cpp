ac03c440ac526769f8b28eb66e36df8f7b080f8c
Ninespring <xiaolongshen.stuff@gmail.com>
2015-07-13 11:06:06 +0800

    Initial Commit

---
+++ Adaboost_On_Android/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Adaboost_On_Android
+//
+//  Created by Ninespring on 15/7/13.
+//  Copyright (c) 2015年 Ninespring. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
